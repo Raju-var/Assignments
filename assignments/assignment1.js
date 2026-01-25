54e9e99491219afddca600fbbaf3c3da867e1110
Raju NL <vardharaju006@gmail.com>
2026-01-25 20:12:29 +0530

    assignments1 using js

---
+++ assignments/assignment1.js
@@ -0,0 +1,63 @@
+// 1. Temperature of a city in degrees Celsius: 25.5
+let citytemp=25.5
+console.log("citytemperature:",citytemp)
+// 2. Whether a customer has placed an order: true or false
+let hasordered=true
+console.log("Customerplacedorde:",hasordered)
+// 3. Person's phone number: "123-456-7890"
+let phonenumber="123456789"
+console.log("phonenumberis:",phonenumber)
+// 4. Amount of money in a customer's bank account: 1000.50
+let balance=1000.50
+console.log("cutsomer bankbalance is:",balance)
+// 5. Person's email address: "john.doe@example.com"
+let email="john.doe@example.com"
+console.log("[erson's email address is:",email)
+// 6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let lat=37.7749, long=-122.4194
+console.log("corinates of lat mad long is",lat,long)
+// 7. Person's marital status: true or false
+let martitalstatus=false
+console.log("personismarried,:",martitalstatus)
+// 8. Person's occupation: "Software Engineer"
+let occupation="software engineer"
+console.log("person's occupation is:",occupation)
+// 9. Person's favourite colour: "Blue"
+let color="blue"
+console.log("persons fav color is:",color)
+// 10.Current year: 2023
+let currentyear=2023
+console.log("currentyear:",currentyear)
+// 11.Number of followers on a social media platform: 1,000,000
+let followers=1000000
+console.log("total followers on instagram:",followers)
+
+// 12.Rating of a movie: 7.5
+let rating=7.5
+console.log("movie rating:",rating)
+// 13.Person's blood type: 'A'
+let bloodgroup='A'
+console.log("Person's bloodgroup is:",)
+// 14.Title of a book: "To Kill a Mockingbird"
+let booktitle="To kill a Mockingbird"
+console.log("Title of the book is :",booktitle)
+// 15.Number of employees in a company: 500
+let emp=500
+console.log("number of employees is:",emp)
+
+// 16.Time of an event: 2:30 PM
+let eventtime="2:30 PM"
+console.log("eventtime is:",eventtime)
+
+// 17.Name of a country: "United States"
+let countryname="united states"
+console.log("Name of a country is:",countryname)
+// 18.Person's eye color: "Brown"
+let eyecolor="Brown"
+console.log("Persons eye color is:",eyecolor)
+// 19.Person's birthplace: "New York City"
+let birthplace="New york city"
+console.log("Persons birthplace:",birthplace)
+// 20. Distance between two cities: 200.5
+let distance=200.5
+console.log("distance between two cities:",distance)
